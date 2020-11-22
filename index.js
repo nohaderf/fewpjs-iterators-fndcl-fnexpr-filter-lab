3df3de5137126949ff5b5d0769c66339d7f314cd
nohaderf <freda.hon@gmail.com>
2020-11-21 20:56:36 -0500

    commit tree b52a5631026afcd8cb5c1247f4feaabff41b2f04

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(match =>
+        match.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(letter =>
+        letter.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver =>
+        driver.name === string 
+    )
+}
